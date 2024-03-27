9cb54937f85a4fd148e7dcf0970e496f959197d9
ksg9482 <ksg930523@gmail.com>
2024-03-27 23:14:31 +0900

    jpa basic start

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,16 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
         //code
+        tx.begin(); //트랜잭션 시작
+        Member member = new Member();
+        member.setId(1l);
+        member.setName("helloA");
+        em.persist(member); //영속화(저장)
+
+        tx.commit(); //트랜잭션 종료하고 내용 커밋
 
         em.close();
         emf.close();
