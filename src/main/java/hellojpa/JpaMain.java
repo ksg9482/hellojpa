8f20418efac76c739cbb5630dd47639e14e4b55b
ksg9482 <ksg930523@gmail.com>
2024-03-29 23:53:46 +0900

    hellojpa

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,11 +13,14 @@ public class JpaMain {
         //이게 정석 코드. 안되면 롤백하고, 끝나면 엔티티메니저 닫아주고.
         tx.begin(); //트랜잭션 시작
         try {
+            //비영속
             Member member = new Member();
             member.setId(1l);
             member.setName("helloA");
 
+            //영속
             em.persist(member); //영속화(저장)
+
             tx.commit(); //트랜잭션 종료하고 내용 커밋
 
         } catch (Exception e) {
